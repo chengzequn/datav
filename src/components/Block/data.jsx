4362336a63000b7ebeed72d8e68f2087b7f166ba
czq <czq@cpic.com>
2020-01-08 15:46:30 +0800

    拖拽

--- src/components/Block/data.jsx
+++ src/components/Block/data.jsx
@@ -1,3 +1,25 @@
-const line = {};
+const line = {
+  data: [
+    { xValue: '1991', yValue: 3, type: 'a' },
+    { xValue: '1992', yValue: 4, type: 'a' },
+    { xValue: '1993', yValue: 3.5, type: 'a' },
+    { xValue: '1994', yValue: 5, type: 'a' },
+    { xValue: '1995', yValue: 4.9, type: 'a' },
+    { xValue: '1996', yValue: 6, type: 'a' },
+    { xValue: '1997', yValue: 7, type: 'a' },
+    { xValue: '1998', yValue: 9, type: 'a' },
+    { xValue: '1999', yValue: 13, type: 'a' },
+
+    { xValue: '1991', yValue: 5, type: 'b' },
+    { xValue: '1992', yValue: 3, type: 'b' },
+    { xValue: '1993', yValue: 5.5, type: 'b' },
+    { xValue: '1994', yValue: 2, type: 'b' },
+    { xValue: '1995', yValue: 3.9, type: 'b' },
+    { xValue: '1996', yValue: 3, type: 'b' },
+    { xValue: '1997', yValue: 5, type: 'b' },
+    { xValue: '1998', yValue: 8, type: 'b' },
+    { xValue: '1999', yValue: 11, type: 'b' },
+  ],
+};
 
 export { line };
